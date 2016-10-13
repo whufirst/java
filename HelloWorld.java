3c56bdf94eca8933fd9f76cbdc5f30b5b1e7c608
whufirst <willhawkingwh@gmail.com>
2016-10-13 11:19:24 +0800

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,4 @@
-// test for push command
+// test for push command again
 
 /*	compiling and executing:
 1.	javac HelloWorld.java	// must have the same name with the class name in the following, then plus suffix .java.
